230a6d734d7896a4d10ffa38508c8f19f3aba6ba
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-05-21 17:23:28 +0200

    add projects commpnnents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { api } from "../../convex/_generated/api";
+import {useMutation, useQuery } from "convex/react";
+
 const X = () => {
+  const projects = useQuery( api.projects.get);
+  const createProject = useMutation(api.projects.create);
+
   return (
-    <div >
-     <Button variant="purple">
-      Hello World
-     </Button>
+    <div className="flex flex-col gap-2 p-4">
+      <Button>
+        Add new
+      </Button>
+      {projects?.map((project) => (
+        <div className="border rounded p-2 flex flex-col" key={project._id}>
+          <p>{project.name}</p>
+          <p>Owner ID: {project.ownerId}</p>
+        </div>
+      ))}
     </div>
   );
 };
