fde3a75dd113c517cf7664de73b22438eed1f334
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-05-21 18:06:26 +0200

    add auth.config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ const X = () => {
 
   return (
     <div className="flex flex-col gap-2 p-4">
-      <Button>
+      <Button onClick={() => createProject({ name: "New Project" })}>
         Add new
       </Button>
       {projects?.map((project) => (
         <div className="border rounded p-2 flex flex-col" key={project._id}>
           <p>{project.name}</p>
           <p>Owner ID: {project.ownerId}</p>
-        </div>
+        </div> 
       ))}
     </div>
   );
