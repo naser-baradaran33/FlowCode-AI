c2bb934aa408389022fff742299dfdf8f87773ed
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-05-22 11:53:38 +0200

    add inngest

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export default function DemoPage() {
 
     const [loading, setLoading] = useState(false);
+    const [loading2, setLoading2] = useState(false);
 
     const handleButtonClick = async () => {
         setLoading(true);
@@ -13,11 +14,22 @@ export default function DemoPage() {
             method: 'POST',
         });
          setLoading(false);
-    }
+    };
+
+    const handleBackground = async () => {
+        setLoading(true);
+        await fetch('/api/demo/background', {
+            method: 'POST',
+        });
+         setLoading(false);
+    };
     return (
         <div className="p-8 space-x-4">
             <Button disabled={loading} onClick={handleButtonClick}>
                 {loading ? 'Loading...' : 'Blocking'}
+            </Button>
+            <Button disabled={loading2} onClick={handleBackground}>
+                {loading ? 'Loading...' : 'Background'}
             </Button>
 
         </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,21 @@
 // src/inngest/functions.ts
+import { generateText } from "ai";
 import { inngest } from "./client";
+import { google } from "@ai-sdk/google";
 
-export const processTask = inngest.createFunction(
-  { id: "process-task", triggers: { event: "app/task.created" } },
-  async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
+export const demoGenerate = inngest.createFunction(
+  {
+    id: "demo-generate",
+    triggers: [{ event: "test/demo.generate" }],
+  },
+  async ({ step }) => {
+    const { text } = await step.run("generate-text", async () => {
+      return await generateText({
+        model: google("gemini-2.5-flash"),
+        prompt: "Write a vegetarian lasagna recipe for 4 people.",
+      });
     });
 
-    await step.sleep("pause", "1s"); 
-
-    return { message: `Task ${event.data.id} complete`, result };
+    return { text };
   }
 );
