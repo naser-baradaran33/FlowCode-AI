54b1085c02a1139056437d23f38de11f27ca0689
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-05-23 22:45:23 +0200

    add finalprompt

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,6 +2,7 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
 import { anthropic } from "@ai-sdk/anthropic";
+import { firecrawl } from "@/lib/firecrawl";
 
 
 const URL_REGEX = /https?:\/\/[^\s]+/g;
@@ -19,12 +20,26 @@ export const demoGenerate = inngest.createFunction(
 
     }) as string[];
 
+    const scrapedContent = await step.run("scrape-urls", async () => {
+     const results = await Promise.all(urls.map(async (url) => {
+        const result = await firecrawl.scrape(url,
+          
+        { formats:["markdown"] },
+        );
+        return result.markdown ?? null;
+      }));
+      return result.filter(Boolean).join("\n\n");
+    });
+
+    const finalPrompt = scrapedContent 
+    ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}` : prompt;
+
 
     await step.run("generate-text", async () => {
       return await generateText({
   model: anthropic('claude-3-haiku-20240307'),
-  prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+  prompt: finalPrompt,
 });
-  });
-  }
+  })
+  },
 );
