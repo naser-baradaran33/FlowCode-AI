94aacd095bd1a4fa8ec3532cc06529e2edf97c31
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-06-13 17:00:28 +0200

    add ky dependency and implement fetcher for AI suggestions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import { Providers } from "@/components/providers";
+
+import "allotment"
 import "./globals.css";
 
 
@@ -35,8 +38,8 @@ export default function RootLayout({
 
         
           <Providers>
-            
             {children}
+            <Toaster />
           </Providers>
     
         
