2d823d564f937b0e08d8bb02828369359b95c808
naser-baradaran33 <danielbaradaran31@gmail.com>
2026-06-30 13:42:51 +0200

    import in ainngest api import&export github

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,15 +1,16 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
-import {  demoError, demoGenerate } from "@/inngest/functions";
 import { processMessage  } from "@/features/conversations/inngest/process-message";
+import { exportToGithub } from "@/features/projects/inngest/export-to-github";
+import { importGithubRepo } from "@/features/projects/inngest/import-github-repo";
 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    demoGenerate,
-    demoError,
     processMessage,
+    importGithubRepo,
+    exportToGithub,
   ],
 });
